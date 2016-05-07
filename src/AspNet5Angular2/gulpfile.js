e8d8cb540c7d84e87137f354969b1e19ec41e2ae
shreedhar <shreedhar@DESKTOP-IDJSPF9>
2016-05-07 15:45:52 +0530

    simple angular2

---
+++ src/AspNet5Angular2/gulpfile.js
@@ -0,0 +1,29 @@
+﻿/// <binding AfterBuild='default' />
+/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+
+gulp.task('default', function () {
+    // place code for your default task here
+    gulp.src([
+      'node_modules/angular2/bundles/js',
+      'node_modules/angular2/bundles/angular2.*.js*',
+      'node_modules/angular2/bundles/angular2-polyfills.js',
+      'node_modules/angular2/bundles/http.*.js*',
+      'node_modules/angular2/bundles/router.*.js*',
+      'node_modules/es6-shim/es6-shim.min.js*',
+      'node_modules/angular2/es6/dev/src/testing/shims_for_IE.js',
+      'node_modules/systemjs/dist/*.*',
+      'node_modules/jquery/dist/jquery.*js',
+      'node_modules/bootstrap/dist/js/bootstrap*.js',
+      'node_modules/rxjs/bundles/Rx.js'
+    ]).pipe(gulp.dest('./wwwroot/libs/'));
+
+    gulp.src([
+      'node_modules/bootstrap/dist/css/bootstrap.css'
+    ]).pipe(gulp.dest('./wwwroot/libs/css'));
+});
+
